59799963ed99bf6e8ab136bcf4bcd9cbdb8c9ae9
huda <115257982+hudahumaira@users.noreply.github.com>
2024-11-26 17:23:18 +0100

    openid implemented

--- src/main/java/Bookstore/Controllers/HomeController.java
+++ src/main/java/Bookstore/Controllers/HomeController.java
@@ -1,7 +1,6 @@
 package Bookstore.Controllers;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,28 +14,33 @@ public class HomeController {
         return ResponseEntity.ok("Welcome to the Bookstore!");
     }
 
+    // @GetMapping("/user")
+    // public String getUser(OAuth2AuthenticationToken authentication) {
+    //     // var groups = (List<String>)authentication.getPrincipal().getAttribute("https://gitlab.org/claims/groups/owner");
+    //     // if(!groups.contains("lsit-ken3239/roles/petkeepers")){
+    //     //     //throw new Exception("Access Denied...");
+    //     // };
+
+    //     var userAttributes = authentication.getPrincipal().getAttributes();
+
+    //     // StringBuilder sb = new StringBuilder();
+    //     // for(var entry : userAttributes.entrySet()){
+    //     //     var s = entry.getKey() + ": " + entry.getValue();
+    //     //     sb.append("\n").append(s);
+    //     // }
+
+    //     return "<pre> \n" +
+    //             userAttributes.entrySet().parallelStream().collect(
+    //                     StringBuilder::new,
+    //                     (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
+    //                     (a, b) -> a.append("\n").append(b)
+    //             ) +
+    //             "</pre>";
+    // }
     @GetMapping("/user")
-    public String getUser(OAuth2AuthenticationToken authentication) throws Exception{
-        var groups = (List<String>)authentication.getPrincipal().getAttribute("https://gitlab.org/claims/groups/owner");
-        if(!groups.contains("lsit-ken3239/roles/petkeepers")){
-            //throw new Exception("Access Denied...");
-        };
-
+    public ResponseEntity<?> getUser(OAuth2AuthenticationToken authentication) {
         var userAttributes = authentication.getPrincipal().getAttributes();
-
-        // StringBuilder sb = new StringBuilder();
-        // for(var entry : userAttributes.entrySet()){
-        //     var s = entry.getKey() + ": " + entry.getValue();
-        //     sb.append("\n").append(s);
-        // }
-
-        return "<pre> \n" +
-                userAttributes.entrySet().parallelStream().collect(
-                        StringBuilder::new,
-                        (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
-                        (a, b) -> a.append("\n").append(b)
-                ) +
-                "</pre>";
+        return ResponseEntity.ok(userAttributes);
     }
 
 }
